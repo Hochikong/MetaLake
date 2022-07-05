2b855fdc226b247446f0ff39d04d7e2d8258d24b
Hochikong <ckhoidea@hotmail.com>
2022-07-05 23:41:09 +0800

    init
    Signed-off-by: Hochikong <ckhoidea@hotmail.com>

---
+++ src/test/java/me/ckhoidea/metalake/MetalakeApplicationTests.java
@@ -0,0 +1,13 @@
+package me.ckhoidea.metalake;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetalakeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

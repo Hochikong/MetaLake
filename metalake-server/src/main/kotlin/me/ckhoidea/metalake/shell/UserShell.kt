69d05f52f44f04e242db3283bb60249e234fe0b9
Hochikong <ckhoidea@hotmail.com>
2022-07-11 18:09:55 +0800

    new shell module
    Signed-off-by: Hochikong <ckhoidea@hotmail.com>

--- metalake-server/src/main/kotlin/me/ckhoidea/metalake/shell/UserShell.kt
+++ metalake-server/src/main/kotlin/me/ckhoidea/metalake/shell/UserShell.kt
@@ -2,6 +2,7 @@ package me.ckhoidea.metalake.shell
 
 import me.ckhoidea.metalake.domain.AuthHashEntity
 import me.ckhoidea.metalake.repository.AuthHashRepository
+import me.ckhoidea.metalake.repository.AuthRepository
 import me.ckhoidea.metalake.repository.LakeBindingRepository
 import me.ckhoidea.metalake.service.AccessKeyValidService
 import me.ckhoidea.metalake.service.GenerateHashService
@@ -16,9 +17,10 @@ import org.springframework.shell.standard.ShellOption
 @ShellComponent
 
 class UserShell(
-    @Autowired val hashService: GenerateHashService,
+    @Autowired val authRepo: AuthRepository,
     @Autowired val authHashRepo: AuthHashRepository,
     @Autowired val lakeBindingRepo: LakeBindingRepository,
+    @Autowired val hashService: GenerateHashService,
     @Autowired val passValidService: PasswordValidService,
     @Autowired val keyValidService: AccessKeyValidService
 ) {
@@ -67,17 +69,80 @@ class UserShell(
         value = """
             List all lakes by main password
             Usage: /ls-lakes -P 1234 
+            Options: -P
         """, key = ["/ls-lakes"], group = "Service"
     )
     fun lsLakes(@ShellOption("-P") password: String) {
         if (passValidService.isValidPassword(password)) {
             val maps = lakeBindingRepo.findAll().map {
                 mapOf(
+                    "ID" to it.id,
                     "AccessKey" to it.accessKey,
                     "DataSource" to it.dataSource,
                     "DataSourceDesc" to it.dataSourceDesc,
                     "DataSourceName" to it.dataSourceName,
                     "PluginUID" to it.pluginUID
+                )
+            }.toList()
+            for (map in maps) {
+                println("--------------------------------------------------------------------")
+                for (entry in map.entries) {
+                    println("${entry.key}: ${entry.value}")
+                }
+                println("--------------------------------------------------------------------")
+                println()
+            }
+
+        }
+    }
+
+    @ShellMethod(
+        value = """
+            List all lakes by main password
+            Usage: /ls-auths -P 1234 
+            Options: -P
+        """, key = ["/ls-auths"], group = "Service"
+    )
+    fun lsAuths(@ShellOption("-P") password: String) {
+        if (passValidService.isValidPassword(password)) {
+            val maps = authRepo.findAll().map {
+                mapOf(
+                    "ID" to it.id,
+                    "AccessKey" to it.accessKey,
+                    "AccessSecret" to it.accessSecret,
+                )
+            }.toList()
+            for (map in maps) {
+                println("--------------------------------------------------------------------")
+                for (entry in map.entries) {
+                    println("${entry.key}: ${entry.value}")
+                }
+                println("--------------------------------------------------------------------")
+                println()
+            }
+
+        }
+    }
+
+    @ShellMethod(
+        value = """
+            List all lakes by main password
+            Usage: /new-lake -P 1234 -K ACCESS_KEY -S jdbc:xxxxx -SN MyData -SD MyData's Content --plugin PLUGIN_ID 
+        """, key = ["/new-lake"], group = "Service"
+    )
+    fun registerLake(
+        @ShellOption("-P") password: String,
+        @ShellOption("-K") accessKey: String,
+        @ShellOption("-S") dataURL: String,
+        @ShellOption("-SN") dataSourceName: String,
+        @ShellOption("-SD") dataSourceDesc: String,
+        @ShellOption("--plugin") pluginID: String
+    ) {
+        if (passValidService.isValidPassword(password)) {
+            val maps = authRepo.findAll().map {
+                mapOf(
+                    "AccessKey" to it.accessKey,
+                    "AccessSecret" to it.accessSecret,
                 )
             }.toList()
             for (map in maps) {
